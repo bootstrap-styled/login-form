4a5eb3f8fae8c754dbf8c16314e97bb84319cbe4
Dimitri Alexander Kopriwa <dimitri.kopriwa@kopaxgroup.com>
2018-07-18 06:13:54 +0700

    fix(ci): fix CI with scripts ci and using @yeutech-lab/rollup-umd-documentation

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,3 +1,3 @@
-const { config } = require('@yeutech/rollup-documentation/lib/styleguide.config.js');
+const { config } = require('@yeutech-lab/rollup-umd-documentation/lib/styleguide.config.js');
 
 module.exports = config;
